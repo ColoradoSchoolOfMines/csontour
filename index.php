3fabc00b596b15616e0d0eadfb3e2757d5a2472f
Morgan Cox <lakewoodlego@gmail.com>
2020-02-03 22:28:07 -0700

    Header finished

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="CS@Mines On Tour">
-	<link rel="stylesheet" type="text/css" href="templateStyle.css">
 	<link rel="stylesheet" type="text/css" href="mainStyle.css">
 
 </head>
     
     <!-- Nav Menu -->
 	<?php include 'onTourHeader.php' ?>
+    
 <body>
 
     <article>
