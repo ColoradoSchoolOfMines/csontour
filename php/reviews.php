38a7195b80fbba6482aa2309d7bed8ee9a232151
Morgan Cox <lakewoodlego@gmail.com>
2020-04-02 18:13:01 -0600

    Fixes

--- php/reviews.php
+++ php/reviews.php
@@ -42,7 +42,7 @@ facts on the occupational outlook for computer science”
 			</p>
 		</section>
         
-        <a href="#team"> <img class="personPicture" src="../images/team2.jpg" alt="team picture"> </a>
+        <a href="#team"> <img src="../images/team2.jpg" alt="team picture"> </a>
 
 	</article>
 
